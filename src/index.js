a4ef86e4b1534e0c0d228648c17a831fbe872fd8
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-10-27 03:52:41 +0100

    📦 NEW: set up fake database api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {fetchTodos} from './api'
 import Root from './components/Root'
 import configureStore from './configureStore'
 
 const store = configureStore()
+
+fetchTodos('completed').then((todos) => console.log(todos))
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'))
