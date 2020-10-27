66dfa14b71536481313d95d8232cf18eaa984aae
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-10-27 03:49:41 +0100

    📦 NEW: wrapping dispatch with log using consogroup lesson 12

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,21 +1,35 @@
-import { throttle } from "lodash";
-import { createStore } from "redux";
-import { loadState, saveState } from "./local-storage";
-import todoApp from "./reducers";
+import {createStore} from 'redux'
+import {loadState} from './local-storage'
+import todoApp from './reducers'
+
+const addLoggingToDispatch = (store) => {
+  /* eslint-disable no-console */
+  const rawDispatch = store.dispatch
+  if (!console.group) {
+    return rawDispatch
+  }
+
+  return (action) => {
+    console.group(action.type)
+    console.log('%c prev state', 'color: gray', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = rawDispatch(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
+  }
+  /* eslint-enable no-console */
+}
 
 const configureStore = () => {
-  const persistedState = loadState();
-  const store = createStore(todoApp, persistedState);
+  const persistedState = loadState()
+  const store = createStore(todoApp, persistedState)
 
-  store.subscribe(
-    throttle(() => {
-      saveState({
-        todos: store.getState().todos,
-      });
-    }, 1000)
-  );
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store)
+  }
 
-  return store;
-};
+  return store
+}
 
-export default configureStore;
+export default configureStore
