2e2dea432a428efd6f3d5621514d2fbe6337ee5a
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-10-27 04:13:03 +0100

    📦 NEW: configure the visibleTodoList component to fetch data using the fake api lesson 14

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,17 +1,50 @@
+import PropTypes from 'prop-types'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import {toggleTodo} from '../actions'
+import {fetchTodos} from '../api'
 import {getVisibleTodos} from '../reducers'
 import TodoList from './TodoList'
 
-const mapStateToTodoListProps = (state, {match}) => {
-  return {
-    todos: getVisibleTodos(state, match.params.filter || 'all'),
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    console.log(this.props)
+    fetchTodos(this.props.filter).then(
+      (todos) => console.log(this.props.filter, todos), // eslint-disable-line no-console
+    )
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.filter !== prevProps.filter) {
+      fetchTodos(this.props.filter).then(
+        (todos) => console.log(this.props.filter, todos), // eslint-disable-line no-console
+      )
+    }
+  }
+
+  render() {
+    return <TodoList {...this.props} />
   }
 }
 
-const VisibleTodoList = withRouter(
-  connect(mapStateToTodoListProps, {onTodoClick: toggleTodo})(TodoList),
+VisibleTodoList.propTypes = {
+  filter: PropTypes.oneOf(['all', 'active', 'completed']).isRequired,
+  receiveTodos: PropTypes.func.isRequired,
+  toggleTodo: PropTypes.func.isRequired,
+}
+
+const mapStateToTodoListProps = (state, {match}) => {
+  const filter = match.params.filter || 'all'
+  return {
+    todos: getVisibleTodos(state, filter),
+    filter,
+  }
+}
+
+// eslint-disable-next-line no-class-assign
+VisibleTodoList = withRouter(
+  connect(mapStateToTodoListProps, {onTodoClick: toggleTodo})(VisibleTodoList),
 )
 
 export default VisibleTodoList
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {fetchTodos} from './api'
 import Root from './components/Root'
 import configureStore from './configureStore'
 
 const store = configureStore()
-
-fetchTodos('completed').then((todos) => console.log(todos))
 
 ReactDOM.render(<Root store={store} />, document.getElementById('root'))
